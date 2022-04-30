fa550f4f81433998dbee316c9f02a3b441200b47
askador <fedosov0912@gmail.com>
2022-04-30 15:20:30 +0300

    logger.error() takes only 1 argument

--- src/handlers/errors.ts
+++ src/handlers/errors.ts
@@ -10,13 +10,13 @@ async function errorHandler(err: BotError<MyContext>) {
   )
   const e = err.error
   if (e instanceof BotError) {
-    logger.error("Error in bot: ", e.ctx)
+    logger.error(`Error in bot: ${e.ctx}`)
   } else if (e instanceof GrammyError) {
-    logger.error("Error in request:", e.description)
+    logger.error(`Error in request: ${e.description}`)
   } else if (e instanceof HttpError) {
-    logger.error("Could not contact Telegram:", e)
+    logger.error(`Could not contact Telegram: ${e}`, )
   } else {
-    logger.error("Unknown error:", e)
+    logger.error(`Unknown error: ${e}`)
   }
 }
 
